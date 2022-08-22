6f59b9f526d8b65a6b5a1312304c901ec690213a
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-22 15:21:19 +0300

    find/break

---
+++ Examples10/Program.cs
@@ -0,0 +1,13 @@
+﻿ int []array = {23,14,3,58,95,12,3,90};
+ int n = array.Length;
+ int find = 3;
+ int index = 0;
+ while(index < n)
+ {
+    if(array[index]== find){
+        Console.WriteLine(index);
+        break;
+    }
+     index ++ ;
+ }
+ 
